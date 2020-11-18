105acb10a25462c099e756876d925f5580b9d8a5
BrunoDesigner <46052756+BrunoDesigner@users.noreply.github.com>
2020-11-18 13:08:56 -0300

    Repetições "do e while"

---
+++ Repetições/repeticoes.js
@@ -0,0 +1,23 @@
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+var cont = 1
+/*
+while (cont <= 6 ) {
+    console.log('Eu to Bem')
+    cont++
+}
+*/
+
+do{
+    console.log('top')
+    cont++
+}while(cont<=6)
+
