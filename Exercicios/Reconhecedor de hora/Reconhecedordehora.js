065c15b4e58fb6d5ff3fa247aa3b7bc65b0e2ed4
BrunoDesigner <46052756+BrunoDesigner@users.noreply.github.com>
2020-11-20 17:22:52 -0300

    Comcluindo Identificador de Horas

--- Exercicios/Reconhecedor de hora/Reconhecedordehora.js
+++ Exercicios/Reconhecedor de hora/Reconhecedordehora.js
@@ -1,10 +1,26 @@
 
 function carregar(){
-    let imagem = window.document.getElementById('foto')
-    let titulo = window.document.getElementById('titulo')
-    let data = new Data()
-    let hora = data.getHours()
-    titulo.innerHTML = `Agora é ${hora} do Dia`
+    let foto = window.document.getElementById('imagem')
+    let txt = window.document.getElementById('titulo')
+    let data = new Date()
+    let dia = data.getDate()
+    hora = data.getHours()
+    txt.innerHTML = `Agora são ${hora} horas do Dia ${dia}`
+    //condicionais
+    
+    if (hora >= 0 && hora < 12){
+        //Bom Dia
+        foto.src='imagens/amanhecer.png'
+        document.body.style.background = '#F7B32B'
+    }else if (hora >= 12 && hora <18){
+        //Boa Tarde
+        foto.src = 'imagens/tarde.png'
+        document.body.style.background = '#FE9920'
+    }else{
+        //Bao Noite
+        foto.src = 'imagens/noite.png'
+        document.body.style.background = '#000000'
+    }
+
+
 }
-
-
